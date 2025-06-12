3b3c19a96d6e609095845e0af718d15e09994a51
Sachit Mittal <sachitmittal073@gmail.com>
2025-06-12 22:18:47 +0530

    Initial Commit

---
+++ Income App/Income_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Income_AppApp.swift
+//  Income App
+//
+//  Created by Sachit Mittal on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Income_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Income AppTests/Income_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Income_AppTests.swift
+//  Income AppTests
+//
+//  Created by Sachit Mittal on 12/06/25.
+//
+
+import Testing
+@testable import Income_App
+
+struct Income_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
