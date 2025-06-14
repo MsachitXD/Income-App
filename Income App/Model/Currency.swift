93291d290255f2e63b0ccf05fecdbf9bf67add15
Sachit Mittal <sachitmittal073@gmail.com>
2025-06-14 12:46:51 +0530

    New Update

---
+++ Income App/Model/Currency.swift
@@ -0,0 +1,8 @@
+//
+//  Currency.swift
+//  Income App
+//
+//  Created by Sachit Mittal on 14/06/25.
+//
+
+import Foundation
---
+++ Income App/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Income App
+//
+//  Created by Sachit Mittal on 14/06/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
