5df8a3c5e4b73811ab6223f2a5d6afc4dfa5edfd
Sachit Mittal <sachitmittal073@gmail.com>
2025-06-15 13:09:48 +0530

    new update

---
+++ TransactionItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TransactionItem+CoreDataClass.swift
+//  Income App
+//
+//  Created by Sachit Mittal on 14/06/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TransactionItem)
+public class TransactionItem: NSManagedObject {
+
+}
